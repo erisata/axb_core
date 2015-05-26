b950e9b7270229f3fd50645c5f67e2a4e9281464
Karolis Petrauskas <k.petrauskas@gmail.com>
2015-05-26 21:46:33 +0300

    axb_supervisor implemented.

--- src/axb_supervisor.erl
+++ src/axb_supervisor.erl
@@ -21,54 +21,73 @@
 %%% on the adapter's operation mode.
 %%%
 -module(axb_supervisor).
+-behaviour(supervisor).
 -compile([{parse_transform, lager_transform}]).
-% TODO: All
-% -init(Args :: term()) -> {ok, SupSpec :: term()} | ignore.
-%
-%
-% %%
-% %%
-% %%
-% start_link() ->
-%     supervisor:start_link().
-%
-%
-% %%
-% %%
-% %%
-% start_child() ->
-%     ok.
-%
-%
-% %%
-% %%
-% %%
-% stop_child() ->
-%     ok.
-%
-%
+-export([start_link/3, start_child/1, stop_child/1]).
+-export([init/1]).
+
+%%% ============================================================================
+%%% Callback definitions.
+%%% ============================================================================
+
+%%
+%%  Corresponds to the `supervisor:init/1` callback.
+%%
+-callback init(
+        Args :: term()
+    ) ->
+        {ok, SupSpec :: term()} |
+        ignore.
 
 
 
-% %%
-% register_flow(NodeName, FlowModule) ->
-%     case axb_flow:describe(NodeName, FlowModule, sup_spec) of
-%         undefined ->
-%             ok;
-%         {ok, ChildSpec} ->
-%             {ok, _ChildPid} = supervisor:start_child(?REF(NodeName), ChildSpec),
-%             ok
-%     end.
-%
-%
-% %%
-% %%
-% %%
-% unregister_flow(NodeName, FlowModule) ->
-%     case supervisor:terminate_child(?REF(NodeName), FlowModule) of
-%         ok ->
-%             ok = supervisor:delete_child(?REF(NodeName), FlowModule),
-%             ok;
-%         {error, not_found} ->
-%             {error, not_found}
-%     end.
+%%% ============================================================================
+%%% API functions.
+%%% ============================================================================
+
+%%
+%%
+%%
+start_link(Name, Module, Args) ->
+    supervisor:start_link(Name, Module, {Module, Args}).
+
+
+%%
+%%  Start or restart a child by given specification.
+%%
+start_child({ChildId, _StartFunc, _Restart, _Shutdown, _Type, _Modules} = StartSpec) ->
+    case supervisor:start_child(?MODULE, StartSpec) of
+        {ok, _Child} ->
+            ok;
+        {error, {already_started, _Child}} ->
+            ok;
+        {error, already_present} ->
+            {ok, _Child} = supervisor:restart_child(?MODULE, ChildId),
+            ok
+    end.
+
+
+%%
+%%  Stop the given child, if exists.
+%%
+stop_child({ChildId, _StartFunc, _Restart, _Shutdown, _Type, _Modules}) ->
+    case supervisor:terminate_child(?MODULE, ChildId) of
+        ok ->
+            ok;
+        {error, not_found} ->
+            ok
+    end.
+
+
+
+%%% ============================================================================
+%%% Callbacks for `supervisor`.
+%%% ============================================================================
+
+%%
+%%
+%%
+init({Module, Args}) ->
+    Module:init(Args).
+
+
